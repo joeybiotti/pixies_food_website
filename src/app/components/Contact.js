ae4b36204bd5e2802a7b3be96dc8fdeadd5cc635
Joey Biotti <joeybiotti@Joeys-MacBook-Pro.local>
2025-02-20 16:30:02 -0500

    margin adjustments

--- src/app/components/Contact.js
+++ src/app/components/Contact.js
@@ -8,9 +8,6 @@ const Contact = () => {
         <p className='mb-1'>
           Get in touch with us for any questions, feedback, or support. We&apos;d love to hear from you!
         </p>
-        <a href='mailto:joey@withthepixies.com' className='text-blue-500 hover:underline'>
-          Email us!
-        </a>
         <div className='flex justify-center my-8'>
           <Image src='/pixie_logo.png' alt='Pixie logo' width={200} height={167} priority className='p-4' />
         </div>
