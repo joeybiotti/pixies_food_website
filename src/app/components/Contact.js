944abc92bd9ea3c3684b5834269107cc915dd4ec
Joey Biotti <joeybiotti@Joeys-MacBook-Pro.local>
2025-02-20 16:25:57 -0500

    made contact.js styling match other comps

--- src/app/components/Contact.js
+++ src/app/components/Contact.js
@@ -1,8 +1,20 @@
+import Image from 'next/image';
+
 const Contact = () => {
   return (
-    <div>
-      <h3>Contact Us!</h3>
-      <p>Reach out to us at [put email address here] or find us on all the socials.</p>
+    <div className='flex flex-col items-center justify-center pt-12'>
+      <div className='mx-auto my-8 p-4 max-w-4xl text-center'>
+        <h3 className='text-2xl font-bold mb-1'>Contact Us</h3>
+        <p className='mb-1'>
+          Get in touch with us for any questions, feedback, or support. We&apos;d love to hear from you!
+        </p>
+        <a href='mailto:joey@withthepixies.com' className='text-blue-500 hover:underline'>
+          Email us!
+        </a>
+        <div className='flex justify-center my-8'>
+          <Image src='/pixie_logo.png' alt='Pixie logo' width={200} height={167} priority className='p-4' />
+        </div>
+      </div>
     </div>
   );
 };
