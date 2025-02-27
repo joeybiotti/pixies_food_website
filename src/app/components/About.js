cd138557c20c15579fe35db0300260252c853cd4
Joey Biotti <joeybiotti@Joeys-MacBook-Pro.local>
2025-02-27 13:58:43 -0500

    replaced svg About.js

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -6,16 +6,16 @@ const About = () => {
       <div className='w-full sm:w-3/4 lg:w-1/2 mx-auto my-4 p-4 text-center'>
         <div className='flex justify-center mb-2'>
           <Image
-            src='/about.svg'
-            alt='Pixie logo'
-            width={600}
-            height={400}
+            src='/aboutus.svg'
+            alt='About Us'
+            width={800}
+            height={100}
             priority
-            className='p-2'
+            className='p-5'
             aria-label='About Us'
           />
         </div>
-        <p className='mt-1 mb-2 text-center px-2'>
+        <p className='mt-5 mb-2 text-center px-2'>
           Pixie is all about creating magical moments for everyone! We’re a family-owned shop bringing fun, accessible,{' '}
           <span className='font-bold text-primaryText'>DELICIOUS</span> vegan treats to little corners of New England.
         </p>
