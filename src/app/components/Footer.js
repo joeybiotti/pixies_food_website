b08f35a6ba7227bd9baa2361075339c1a94d1b8a
Joey Biotti <joeybiotti4@gmail.com>
2025-03-06 16:15:42 -0500

    removed unused social media links from footer.js

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,43 +1,26 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { faInstagram, faSquareFacebook, faBluesky, faTiktok } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram, faBluesky } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
     <footer className='bg-black-800 text-white py-4'>
-      <Head>
-        <title>Pixie Food Company - Footer</title>
-        <meta name='description' content='Pixie Food Company - Footer' />
-      </Head>
       <div className='flex flex-col items-center'>
         <p className='text-xs mb-2'>&copy; {new Date().getFullYear()} Pixie Food Company. All rights reserved.</p>
         <div className='flex gap-4'>
-          {/* <Link href='mailto:your-email@example.com' className='text-lg hover:text-blue-500' aria-label='Email us'>
-            <FontAwesomeIcon icon={faEnvelope} className='text-primaryText' />
-          </Link> */}
           <Link
             href='https://www.instagram.com/pixie.food.co/'
             className='text-lg hover:text-blue-500'
             aria-label='Instagram'>
             <FontAwesomeIcon icon={faInstagram} className='text-primaryText' />
           </Link>
-          {/* <Link
-            href='https://www.facebook.com/yourprofile'
-            className='text-lg hover:text-blue-500'
-            aria-label='Facebook'>
-            <FontAwesomeIcon icon={faSquareFacebook} className='text-primaryText' />
-          </Link> */}
           <Link
             href='https://bsky.app/profile/pixiefood.bsky.social'
             className='text-lg hover:text-blue-500'
             aria-label='Bluesky'>
             <FontAwesomeIcon icon={faBluesky} className='text-primaryText' />
           </Link>
-          {/* <Link href='https://www.tiktok.com/@yourprofile' className='text-lg hover:text-blue-500' aria-label='TikTok'>
-            <FontAwesomeIcon icon={faTiktok} className='text-primaryText' />
-          </Link> */}
         </div>
       </div>
     </footer>
