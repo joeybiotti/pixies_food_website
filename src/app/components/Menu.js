ded5dd558853faa68e44d58bc3551542d931638e
Joey Biotti <joeybiotti4@gmail.com>
2025-08-05 22:24:30 -0400

    feat: Add modal for Vegan Nite Market Menu and update Menu component
    - Implemented a modal in MainContent.js to display the Vegan Nite Market Menu with animations using Framer Motion.
    - Updated Menu.js to replace the seasonal menu image with a new full menu image and refactored modal implementation for consistency.
    - Adjusted padding and layout in page.js for improved responsiveness and visual appeal.

--- src/app/components/Menu.js
+++ src/app/components/Menu.js
@@ -32,7 +32,7 @@ const Menu = () => {
           <div className='cursor-pointer' onClick={openModal}>
             <div className='w-full overflow-x-auto max-w-full'>
               <Image
-                src='/seasonalmenu.png'
+                src='/fullmenu.svg'
                 alt='Menu'
                 width={1000}
                 height={800}
@@ -45,57 +45,61 @@ const Menu = () => {
         </div>
       </div>
 
-      <AnimatePresence>
-        {isModalOpen && (
-          <motion.div
-            key='modal-overlay'
-            className='fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm z-50'
+<AnimatePresence>
+  {isModalOpen && (
+    <motion.div
+      key='modal-overlay'
+      className='fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm z-50'
+      onClick={closeModal}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.3 }}
+    >
+      <motion.div
+        className='fixed inset-0 flex items-center justify-center z-50'
+        initial={{ scale: 0.98, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        exit={{ scale: 0.98, opacity: 0 }}
+        transition={{ duration: 0.3 }}
+      >
+        <motion.div
+          className='bg-white w-full max-w-6xl h-screen relative shadow-xl overflow-hidden'
+          onClick={(e) => e.stopPropagation()}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.3 }}
+        >
+          <button
+            className='absolute top-4 right-4 text-3xl text-black bg-white p-3 rounded-full z-50 hover:bg-black hover:text-white transition-colors duration-300'
             onClick={closeModal}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.3 }}>
-            <motion.div
-              className='fixed inset-0 flex items-center justify-center z-50'
-              initial={{ scale: 0.8, opacity: 0 }}
-              animate={{ scale: 1, opacity: 1 }}
-              exit={{ scale: 0.8, opacity: 0 }}
-              transition={{ duration: 0.3 }}>
-              <motion.div
-                className='bg-gray-500 p-4 w-full max-w-7xl h-4/5 relative rounded-md shadow-lg'
-                onClick={(e) => e.stopPropagation()}
-                style={{ overflow: 'hidden' }}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                transition={{ duration: 0.3 }}>
-                <button
-                  className='absolute top-4 right-4 text-3xl text-black bg-primaryText p-3 rounded-full z-50 hover:text-white transition-colors duration-300'
-                  onClick={closeModal}
-                  aria-label='Close Modal'>
-                  &times;
-                </button>
+            aria-label='Close Modal'
+          >
+            &times;
+          </button>
 
-                <div className='w-full h-full overflow-auto'>
-                  <Image
-                    src='/seasonalmenu.png'
-                    alt='Menu'
-                    width={1200}
-                    height={1000}
-                    priority
-                    className='object-contain w-full rounded-lg'
-                    style={{
-                      transform: 'scale(1.1)',
-                      transition: 'transform 0.3s ease-in-out',
-                    }}
-                    aria-label='Menu'
-                  />
-                </div>
-              </motion.div>
-            </motion.div>
-          </motion.div>
-        )}
-      </AnimatePresence>
+          <div className='w-full h-full overflow-auto p-4'>
+            <Image
+              src='/fullmenu.svg'
+              alt='Menu'
+              width={1200}
+              height={1500}
+              priority
+              className='object-contain w-full rounded-lg'
+              style={{
+                transform: 'scale(1)',
+                transition: 'transform 0.3s ease-in-out',
+              }}
+              aria-label='Menu'
+            />
+          </div>
+        </motion.div>
+      </motion.div>
+    </motion.div>
+  )}
+</AnimatePresence>
+
     </div>
   );
 };
