e5e647e8e37b21f3b719a5f96ff92845e34c8a4e
Joey Biotti <joeybiotti4@gmail.com>
2026-05-02 14:52:35 -0400

    update catering component & replaced img

--- src/app/components/Catering.js
+++ src/app/components/Catering.js
@@ -3,30 +3,18 @@ import Image from 'next/image';
 
 const Catering = () => {
   return (
-    <div className='flex flex-col items-center justify-start pt-2'>
+    <div className='flex flex-col items-center justify-start pt-0'>
       <Head>
         <title>Pixie Food Company - Catering</title>
         <meta name='description' content='Pixie Food Company - Catering' />
       </Head>
-      <div className='w-full sm:w-3/4 lg:w-1/2 mx-auto my-4 p-4 text-center'>
-        <div className='flex justify-center mb-2'>
-          <Image
-            src='/catering.svg'
-            alt='Catering'
-            width={800}
-            height={100}
-            priority
-            className='p-5'
-            aria-label='Catering'
-          />
-        </div>
         <div className='flex justify-center'>
           <picture>
-            <source media='(max-width: 768px)' srcSet='/catering_mobile.svg' />
+            <source media='(max-width: 768px)' srcSet='/catering_2.svg' />
             <Image
-              src='/catering_announcement.svg'
+              src='/catering_2.svg'
               alt='Catering Announcement'
-              width={1200}
+              width={800}
               height={300}
               priority
               className='p-5 w-full h-auto'
@@ -35,7 +23,6 @@ const Catering = () => {
           </picture>
         </div>
       </div>
-    </div>
   );
 };
 
